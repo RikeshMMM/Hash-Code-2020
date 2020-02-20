85a8aa9196b98b687bdf3de46512c4f51ad69a2f
Rikesh Makwana <rikesh.makwana@gmail.com>
2020-02-20 22:12:40 +0400

    Add library and books

---
+++ Hash-Code-2020/src/Library.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Library {
+	int id;
+	int noOfBooks;
+	ArrayList<Book> Book = new ArrayList<Book>();
+	int signUpTime;
+	int booksPerDay;
+	boolean isScanned;
+}
