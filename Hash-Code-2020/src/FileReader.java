98071cb4bfafcd63d7edb40c440c80d22d09055c
m7azeem <m7azeem@gmail.com>
2020-02-20 21:10:02 +0400

    add basic file reader

---
+++ Hash-Code-2020/src/FileReader.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileReader {
+
+    static String fileName = "";
+    static String separator = "";
+
+    public static void readFile() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+
+            // read line by line
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                stringBuilder.append(line).append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
