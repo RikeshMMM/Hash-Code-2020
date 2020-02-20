243434fd3df7ed5114b056c35b7801b8badd1a8b
m7azeem <m7azeem@gmail.com>
2020-02-20 21:40:16 +0400

    add output file/writer

--- Hash-Code-2020/src/FileUtils.java
+++ Hash-Code-2020/src/FileUtils.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class FileUtils {
 
-    static String fileName = "input.txt";
+    static String inputFileName = "input.txt";
     static String separator = "";
+
+    static String outputFileName = "output.txt";
+
 
     public static void readFile() {
 
         StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(inputFileName))) {
 
             // read line by line
             String line;
@@ -22,5 +27,13 @@ public class FileUtils {
             e.printStackTrace();
         }
         System.out.println(stringBuilder.toString());
+    }
+
+    public static void writeFile(ArrayList<String> lines) {
+        try {
+            Files.write(Paths.get(outputFileName), lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Hash-Code-2020/src/Main.java
+++ Hash-Code-2020/src/Main.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
 
 		FileUtils.readFile();
+
+		ArrayList<String> lines = new ArrayList<>();
+		lines.add("hellooo");
+		FileUtils.writeFile(lines);
 	}
 }
